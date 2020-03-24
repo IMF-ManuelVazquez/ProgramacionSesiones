1d27722d7e5dca71272d504362f904180dff7225
ManuelVazquezEnriquez <mvazquez@profesores.imf.com>
2020-03-24 12:02:11 +0100

    Update24_03_2020

--- PRESENCIAL/boletin5/MainPruebas.java
+++ PRESENCIAL/boletin5/MainPruebas.java
@@ -6,26 +6,31 @@ public class MainPruebas {
 
 	public static void main(String[] args) {
 		
-		Ej8JuegoAdivinar juego = new Ej8JuegoAdivinar();
-		//Ej8JuegoAdivinar juego2 = new Ej8JuegoAdivinar(60);
-		
-//		System.out.println(juego.nuevoIntento(50));
-//		System.out.println(juego.nuevoIntento(70));
-//		System.out.println(juego.nuevoIntento(60));
+//		Ej8JuegoAdivinar juego = new Ej8JuegoAdivinar();
+//		//Ej8JuegoAdivinar juego2 = new Ej8JuegoAdivinar(60);
 //		
-//		System.out.println(juego);
+////		System.out.println(juego.nuevoIntento(50));
+////		System.out.println(juego.nuevoIntento(70));
+////		System.out.println(juego.nuevoIntento(60));
+////		
+////		System.out.println(juego);
+////		
+////		juego.nuevaPartida();
 //		
-//		juego.nuevaPartida();
-		
-		// Una interfaz donde el usuario va metiendo numeros hasta que reciba el "ACERTASTES".
-		Scanner entradaEscaner = new Scanner(System.in);
-		String respuesta="";
-		do {
-			System.out.println("Usuario introduzca un numero");
-			respuesta = juego.nuevoIntento(entradaEscaner.nextInt());
-			System.out.println("resultado: "+respuesta);
-		}while(respuesta!="ACERTASTES");
+//		// Una interfaz donde el usuario va metiendo numeros hasta que reciba el "ACERTASTES".
+//		Scanner entradaEscaner = new Scanner(System.in);
+//		String respuesta="";
+//		do {
+//			System.out.println("Usuario introduzca un numero");
+//			respuesta = juego.nuevoIntento(entradaEscaner.nextInt());
+//			System.out.println("resultado: "+respuesta);
+//		}while(respuesta!="ACERTASTES");
 
+		Ej9ConversorEuros.convertirDivisas(222,Ej9ConversorEuros.CONVERTIR_LIBRA);
+		Ej9ConversorEuros.convertirDivisas(100, Ej9ConversorEuros.CONVERTIR_YEN);
+		Ej9ConversorEuros.convertirDivisas(100, "AFDSFA");
+		
+		Ej9ConversorEuros.convertirDivisas(100);
 	
 
 	}
